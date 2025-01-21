100a18d49473778375c3b8fa76ed96149f7659ec
lalitcap23 <lalitrajput232002@gmail.com>
2025-01-21 19:50:26 +0530

    testing purpose only

--- src/main.rs
+++ src/main.rs
@@ -26,5 +26,5 @@ fn main() {
     input.get_ref().metadata().unwrap().len()
     );
     println!("Target len: {:?}", output.metadata().unwrap().len());
-    println!("Elapsed: {:?}", start.elapsed())ls a;
+    println!("Elapsed: {:?}", start.elapsed());
 }
